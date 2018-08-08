5e0928624613bc94f07ce295599ea141e18362ae
ilonaurlik <37349626+ilonaurlik@users.noreply.github.com>
2018-08-08 11:31:02 +0200

    Added description.

--- Calclator/src/Main.java
+++ Calclator/src/Main.java
@@ -1,3 +1,14 @@
+/**
+ * Using the calculator, you can convert an Arabic number to Roman, or vice versa.
+ * The program can do the following calculations:
+ * - addition
+ * - subtraction
+ * - multiplication
+ * - division
+ * - exponentiation.
+ * Activities can be performed on Roman, Arabic or mixed numbers.
+ */
+
 public class Main {
 	public static void main(String[] args) {
 		Calculator calculator = new Calculator();
